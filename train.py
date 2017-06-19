1f4873761580c146031fca7d00d0eea18497369c
zqiao <zqiao@ebay.com>
2017-06-19 09:52:13 +0800

    first commit

--- train.py
+++ train.py
@@ -115,7 +115,7 @@ with tf.Graph().as_default():
         saver = tf.train.Saver(tf.global_variables())
 
         # Initialize all variables
-        sess.run(tf.global_variables_initializer(),max_to_keep=FLAGS.num_checkpoints)
+        sess.run(tf.global_variables_initializer())
 
         def train_step(x_batch, y_batch):
             """
